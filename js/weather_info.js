d83eb65268b198a7b1d4180a96afe17865bab00a
Aina2bean <77223940+Aina2bean@users.noreply.github.com>
2024-02-02 22:47:07 +0900

    Add files via upload

--- js/weather_info.js
+++ js/weather_info.js
@@ -14,60 +14,69 @@ function requestAPI () {
             let today_temp_min = data.daily.temperature_2m_min[0];
             let weekly_temp_weather = data.daily.weathercode.slice(1);
             console.log(weekly_temp_weather);
-            switch (today_weather) {
-                case 0:
-                    weather_now.innerHTML = '晴れ';
-                    break;
-                case 1:
-                case 2:
-                case 3:
-                    weather_now.innerHTML = '晴れ時々曇り';
-                    break;
-                case 45:
-                case 48:
-                    weather_now.innerHTML = '霧';
-                    break;
-                case 51:
-                case 55:
-                    weather_now.innerHTML = '霧雨';
-                    break;
-                case 56:
-                case 57:
-                    weather_now.innerHTML = '霧雨';
-                    break;
-                case 61:
-                case 63:
-                case 65:
-                    weather_now.innerHTML = '雨';
-                    break;
-                case 66:
-                case 67:
-                    weather_now.innerHTML = '強い雨';
-                    break;
-                case 71:
-                case 73:
-                case 75:
-                    weather_now.innerHTML = '雪';
-                    break;
-                case 77:
-                    weather_now.innerHTML = '雪';
-                    break;
-                case 80:
-                case 81:
-                case 82:
-                    weather_now.innerHTML = 'にわか雨';
-                    break;
-                case 85:
-                case 86:
-                    weather_now.innerHTML = '雪';
-                    break;
-                case 95:
-                    weather_now.innerHTML = '雷雨';
-                    break;
-                case 96:
-                    weather_now.innerHTML = '雷雨';
-                    break;
+
+            function weather_code(result){
+                switch (result) {
+                    case 0:
+                        //weather_now.innerHTML = '晴れ';
+                        return '晴れ';
+                        //break;
+                    case 1:
+                    case 2:
+                    case 3:
+                        //weather_now.innerHTML = '晴れ時々曇り';
+                        return '晴れ時々曇り';
+                        //break;
+                    case 45:
+                    case 48:
+                        //weather_now.innerHTML = '霧';
+                        return '霧';
+                        //break;
+                    case 51:
+                    case 53:
+                    case 55:
+                    case 56:
+                    case 57:
+                        //weather_now.innerHTML = '霧雨';
+                        return '霧雨';
+                        //break;
+                    case 61:
+                    case 63:
+                    case 65:
+                        //weather_now.innerHTML = '雨';
+                        return '雨';
+                        //break;
+                    case 66:
+                    case 67:
+                        //weather_now.innerHTML = '強い雨';
+                        return '強い雨';
+                        //break;
+                    case 71:
+                    case 73:
+                    case 75:
+                    case 77:
+                        //weather_now.innerHTML = '雪';
+                        return '雪';
+                        //break;
+                    case 80:
+                    case 81:
+                    case 82:
+                        //weather_now.innerHTML = 'にわか雨';
+                        return 'にわか雨';
+                        //break;
+                    case 85:
+                    case 86:
+                        //weather_now.innerHTML = '雪';
+                        return '雪';
+                        //break;
+                    case 95:
+                    case 96:
+                        //weather_now.innerHTML = '雷雨';
+                        return '雷雨';
+                        //break;
+                }
             }
+            weather_now.innerHTML = weather_code(today_weather);
 
             // 高気温キャンセル危惧
             if(today_temp_max > 29){
@@ -78,9 +87,12 @@ function requestAPI () {
             temp_max.innerHTML = today_temp_max + '℃';
             temp_min.innerHTML = today_temp_min + '℃';
 
-            for(let i = 0; i <= 5; i++){
+            // 週間の天気予報
+            for(let i = 1; i <= 5; i++){
                 let weekly_element = document.createElement('div');
-                weekly_element.innerHTML = '<div>' + weekly_temp_weather[i] + '</div>';
+                let weekly_date = (today.getMonth() + 1) + '/' + (today.getDate() + i);
+
+                weekly_element.innerHTML = '<div>' + weekly_date + weather_code(weekly_temp_weather[i]) + '</div>';
                 document.getElementById('weather_weekly').appendChild(weekly_element);
             }
         });
